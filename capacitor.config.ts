8f8917aefd24ffd9c4bebb5958935710da22a162
Dabs-Cedric <tsankera@net-c.com>
2024-07-24 10:13:03 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gestionion',
+  webDir: 'www'
+};
+
+export default config;
